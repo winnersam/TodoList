c30cc3aa48623589211d3e5d5b964bc19c805eb1
WinnerSam <winnersam@gmail.com>
2016-11-21 14:19:35 +0200

    Programmed add logic

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 //Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-//Click on X to delete Todo
-$("span").click(function(event){
+//Click on X to delete Todo 
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		//Grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+	}
 });
