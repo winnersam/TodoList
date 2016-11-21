4b428a83480ddd2941caea6e569918d196ee3c12
WinnerSam <winnersam@gmail.com>
2016-11-21 12:13:32 +0200

    Reduced code Mark Complation

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,4 @@
 //Check Off Specific Todos By Clicking
 $("li").click(function(){
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		$(this).css({
-		color: "black",
-		textDecoration: "none"
-	});
-	}
-	else {
-		$(this).css({
-		color: "gray",
-		textDecoration: "line-through"
-	});
-	}
-
+	$(this).toggleClass("completed");
 });
